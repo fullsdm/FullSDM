d71a667552a01a82fad87a5905a33984ed68f78f
fullsdmdeveloper <fullsdmdeveloper@gmail.com>
2024-09-08 20:26:45 -0500

    send

--- projects/tickets/script.js
+++ projects/tickets/script.js
@@ -2,12 +2,10 @@ const dialog = document.getElementById("myDialog");
 const openDialogButton = document.getElementById("openDialog");
 const closeDialogButton = document.getElementById("closeDialog");
 
-// Abrir el diálogo como modal centrado
 openDialogButton.addEventListener("click", () => {
   dialog.showModal();
 });
 
-// Cerrar el diálogo
 closeDialogButton.addEventListener("click", () => {
   dialog.close();
 });
