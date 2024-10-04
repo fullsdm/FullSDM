e9cca637d2b0ca2b4d9cb5cd010c98f034efffae
fullsdmdeveloper <fullsdmdeveloper@gmail.com>
2024-10-04 15:46:52 -0500

    send

---
+++ projects/calculator/script.js
@@ -0,0 +1,13 @@
+document.getElementById("sumar").addEventListener("click", function () {
+
+    let num_1 = parseInt(document.getElementById("num_1").value);
+    let num_2 = parseInt(document.getElementById("num_2").value);
+
+    let suma = num_1 + num_2;
+
+    document.getElementById("resultado").innerHTML = "El resultado es: "
+        + suma;
+
+    alert("La suma de los numeros es: " + suma);
+
+})
