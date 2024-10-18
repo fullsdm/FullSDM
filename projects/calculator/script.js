1b3a13bf6f36f774ae506c4fef16ec2c6055f99c
fullsdmdeveloper <fullsdmdeveloper@gmail.com>
2024-10-18 07:37:37 -0500

    send

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -5,31 +5,35 @@ document.getElementById("sumar").addEventListener("click", function () {
   let operacion = parseInt(document.getElementById("operacion").value);
   let resultado = 0;
 
-  if (operacion == 1) {
-    resultado = "Por favor selecione una operacion";
-    document.getElementById("resultado").innerHTML = resultado;
-    document.getElementById("resultado").style.color = "#f00";
-  } else if (operacion == 2) {
-    resultado = num_1 + num_2;
-    document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
-    document.getElementById("resultado").style.color = "#000";
-  } else if (operacion == 3) {
-    resultado = num_1 - num_2;
-    document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
-    document.getElementById("resultado").style.color = "#000";
-  } else if (operacion == 4) {
-    resultado = num_1 * num_2;
-    document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
-    document.getElementById("resultado").style.color = "#000";
-  };
-  
-  if (operacion == 5) {
-    resultado = num_1 / num_2;
-    document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
-    document.getElementById("resultado").style.color = "#000";
-  } else if (resultado == Infinity) {
+  if (resultado == Infinity) {
     resultado = "El valor de uno de los numeros no puede ser CERO";
     document.getElementById("resultado").innerHTML = resultado;
     document.getElementById("resultado").style.color = "#f00";
+  } else {
+    if (operacion == 1) {
+      resultado = "Por favor selecione una operacion";
+      document.getElementById("resultado").innerHTML = resultado;
+      document.getElementById("resultado").style.color = "#f00";
+    } else if (operacion == 2) {
+      resultado = num_1 + num_2;
+      document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
+      document.getElementById("resultado").style.color = "#000";
+    } else if (operacion == 3) {
+      resultado = num_1 - num_2;
+      document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
+      document.getElementById("resultado").style.color = "#000";
+    } else if (operacion == 4) {
+      resultado = num_1 * num_2;
+      document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
+      document.getElementById("resultado").style.color = "#000";
+    } else if (operacion == 5) {
+      resultado = num_1 / num_2;
+      document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
+      document.getElementById("resultado").style.color = "#000";
+    } else if (resultado == Infinity) {
+      resultado = "El valor de uno de los numeros no puede ser CERO";
+      document.getElementById("resultado").innerHTML = resultado;
+      document.getElementById("resultado").style.color = "#f00";
+    }
   }
 });
