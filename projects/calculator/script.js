f746a30cab036e72eb5a550e3f698947970d72bf
fullsdmdeveloper <fullsdmdeveloper@gmail.com>
2024-10-18 07:07:19 -0500

    send

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -2,7 +2,7 @@ document.getElementById("sumar").addEventListener("click", function () {
   let num_1 = parseInt(document.getElementById("num_1").value);
   let num_2 = parseInt(document.getElementById("num_2").value);
 
-  let operacion = parseInt(document, getElementById("operacion").value);
+  let operacion = parseInt(document.getElementById("operacion").value);
   let resultado = 0;
 
   if (operacion == 1) {
