470bb64d90875fcdea890483206ef819a59d7cb9
fullsdmdeveloper <fullsdmdeveloper@gmail.com>
2024-10-18 07:12:52 -0500

    send

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -5,16 +5,18 @@ document.getElementById("sumar").addEventListener("click", function () {
   let operacion = parseInt(document.getElementById("operacion").value);
   let resultado = 0;
 
-  if (operacion == 1) {
+  if (operacion == 2) {
     resultado = num_1 + num_2;
-  } else if (operacion == 2) {
-    resultado = num_1 - num_2;
   } else if (operacion == 3) {
+    resultado = num_1 - num_2;
+  } else if (operacion == 4) {
     resultado = num_1 * num_2;
   } else if (operacion == 5) {
     resultado = num_1 / num_2;
   } else if (resultado == Infinity) {
-    resultado = "El valor de uno de los numeros no puede ser CERO"
+    resultado = "El valor de uno de los numeros no puede ser CERO";
+  } else if (resultado == 1) {
+    resultado = "Por favor selecione una operacion";
   }
 
   document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
