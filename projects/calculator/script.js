38984328f22191cf105a425aa55ba29442394810
fullsdmdeveloper <fullsdmdeveloper@gmail.com>
2024-10-18 07:06:32 -0500

    send

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -15,7 +15,7 @@ document.getElementById("sumar").addEventListener("click", function () {
     resultado = num_1 / num_2;
   } else if (operacion == Infinity) {
     resultado = "Un numero no puedetener el valor de Cero";
-  }
+  };
 
   document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
 });
